e9160e7481dbc907522902582ef745a5d5ef55d3
seeflood <349895584@qq.com>
2022-10-14 16:35:39 +0800

    modify notify.js
    Signed-off-by: seeflood <349895584@qq.com>

--- src/notify.js
+++ src/notify.js
@@ -2,10 +2,18 @@ const sender = require("./dao/dingtalk");
 
 sender.sendMarkdown(
   [
-    // "https://oapi.dingtalk.com/robot/send?access_token=Your group token",
+    // The webhook url of your dingtalk bot. For example:
+    "https://oapi.dingtalk.com/robot/send?access_token=111111111111",
   ],
+
+  // Here's what you want your bot to say.
+  // uid1 and uid2 is the users' id you want to @
   "@uid1 @uid2 Hi, 关于应用集成 Layotto，进展如何了呀，有遇到啥问题么？ \n",
+
+  // The title of your message
   "title",
-  ["uid1","uid2"],
+
+  // uid1 and uid2 is the users' id you want to @
+  ["uid1", "uid2"],
   true
 );
