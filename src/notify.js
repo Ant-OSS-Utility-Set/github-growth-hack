e69a80f60e1d61dfc05c5dc19c273921efd49dc0
zhaoxuanqing <z17614192507@163.com>
2023-05-31 11:29:24 +0800

    fix:token

--- src/notify.js
+++ src/notify.js
@@ -3,7 +3,7 @@ const sender = require("./dao/dingtalk");
 sender.sendMarkdown(
   [
     // The webhook url of your dingtalk bot. For example:
-    "https://oapi.dingtalk.com/robot/send?access_token=0bf65479caed4a82735752ce39c8a9fa89e4fced2204f2e413faad81738e1d00",
+    "https://oapi.dingtalk.com/robot/send?access_token=111111",
   ],
 
   // Here's what you want your bot to say.
